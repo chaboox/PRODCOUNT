9df356928913f351f86325a1e03e35c4cc2abe43
Adam <chaboox@gmail.com>
2019-08-23 13:26:51 +0100

    Add recycle view with example.

---
+++ app/src/main/java/com/example/productcount/AddProductActivity.kt
@@ -0,0 +1,12 @@
+package com.example.productcount
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddProductActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_product)
+    }
+}
